8ed7d85020b51fd59a430a59397bfc202dd13fa6
Affan Hashmi <saxondigitech@gmail.com>
2023-07-14 20:53:31 +0500

    Filters created

---
+++ src/screens/Filters.js
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import { View, Image, Text, SafeAreaView, StyleSheet, TouchableOpacity, ScrollView } from 'react-native'
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from "react-native-responsive-screen"
+import MaterialIcon from 'react-native-vector-icons/MaterialIcons'
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
+
+import * as colors from "../utilities/colors"
+import * as fonts from "../utilities/fonts"
+
+import { Searchbar, TouchableRipple } from 'react-native-paper'
+
+
+const Filters = () => {
+
+}
+const styles = StyleSheet.create({
+  
+})
+export default Filters
